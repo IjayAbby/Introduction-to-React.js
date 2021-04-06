7dedd3df5f1fbaa4b8d81d19635c081c010a5678
Ijay Abby <inyangasi4@gmail.com>
2021-04-06 16:10:24 +0300

    Meet another react component

--- src/App.js
+++ src/App.js
@@ -28,22 +28,27 @@ function App() {
       <input id="search" type="text" />
 
       <hr />
-
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
